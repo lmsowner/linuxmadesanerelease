f58053b877846e5475cd7f88387b30bb16a87d2c
richard linux <richard@it-dev.co.uk>
2026-09-24 11:32:14 +0100

    Publish CE source v2026.09.24.11.35 from e9527b7011cd

--- src/LinuxMadeSane.Application/Contracts/HomeLab/HomeLabCatalog.cs
+++ src/LinuxMadeSane.Application/Contracts/HomeLab/HomeLabCatalog.cs
@@ -202,7 +202,7 @@ public static class HomeLabCatalog
                     "web",
                     HomeLabClientRoutingStrategy.Auto,
                     "/stremio",
-                    new HomeLabReverseProxyManifest(HomeLabBasePathSupportMode.None),
+                    new HomeLabReverseProxyManifest(HomeLabBasePathSupportMode.Transparent),
                     new HomeLabEndpointCapabilities(Streaming: true, RangeRequests: true, WebSockets: true, Uploads: true, LongLivedRequests: true)),
                 [new HomeLabClientAccessManifest(
                     true,
